255e4a6b682b8ba25ee14092a1f229bbe6f88b22
leandrorj <leandro__brasil@hotmail.com>
2018-12-17 11:47:31 -0300

    foi adicionado 1 coluna com uma imagem e 2 textField

--- lib/main.dart
+++ lib/main.dart
@@ -28,6 +28,25 @@ class _HomeState extends State<Home> {
           )
         ],
       ),
+      backgroundColor: Colors.white,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Icon(Icons.person_outline, size: 120.0, color: Colors.green),
+          TextField(
+            keyboardType: TextInputType.number,
+            decoration: InputDecoration(
+                labelText: "Peso (Kg)",
+                labelStyle: TextStyle(color: Colors.green)),
+            ),
+          TextField(
+            keyboardType: TextInputType.number,
+            decoration: InputDecoration(
+                labelText: "Altura (cm)",
+                labelStyle: TextStyle(color: Colors.green)),
+            ),
+        ],
+      ),
     );
   }
 }
