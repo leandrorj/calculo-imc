c2b8df1f8ca9aa61a0be9b2424bea9ed72453d67
leandrorj <leandro__brasil@hotmail.com>
2018-12-17 14:47:53 -0300

    criacao dos controller e metodo de calcular IMC e resetar dados

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     home: Home(),
+    debugShowCheckedModeBanner: false,
   ));
 }
 
@@ -12,6 +13,40 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  String _textInfo = "Informe seus dados!";
+
+  TextEditingController weightController = TextEditingController();
+  TextEditingController heightController = TextEditingController();
+
+  void _resetFields(){
+    weightController.text = "";
+    heightController.text = "";
+    _textInfo = "Informe seus dados!";
+  }
+
+  void _calculate(){
+    setState(() {
+      double peso = double.parse(weightController.text);
+      double altura = double.parse(heightController.text) / 100;
+      double imc = peso / (altura * altura);
+
+      if(imc < 16.6){
+        _textInfo = "Abaixo do Peso (${imc.toStringAsPrecision(4)})";
+      } else if (imc >= 18.6 && imc < 24.9){
+        _textInfo = "Peso ideal (${imc.toStringAsPrecision(4)})";
+      } else if (imc >= 24.9 && imc < 29.9){
+        _textInfo = "Levemente acima do peso (${imc.toStringAsPrecision(4)})";
+      } else if (imc >= 29.9 && imc < 34.9){
+        _textInfo = "Obesidade Grau I (${imc.toStringAsPrecision(4)})";
+      } else if (imc >= 34.9 && imc < 39.9){
+        _textInfo = "Obesidade Grau II (${imc.toStringAsPrecision(4)})";
+      } else if (imc >= 40){
+        _textInfo = "Obesidade Grau III (${imc.toStringAsPrecision(4)})";
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,7 +57,9 @@ class _HomeState extends State<Home> {
         actions: <Widget>[
           IconButton(
             icon: Icon(Icons.refresh),
-            onPressed: () {},
+            onPressed: () {
+              _resetFields();
+            },
           )
         ],
       ),
@@ -35,9 +72,10 @@ class _HomeState extends State<Home> {
           children: <Widget>[
             Icon(Icons.person_outline, size: 120.0, color: Colors.green),
             Padding(
-              padding: EdgeInsets.only(left: 0.0, top: 10.0, right: 0.0, bottom: 10.0),
+              padding: EdgeInsets.only(
+                  left: 0.0, top: 10.0, right: 0.0, bottom: 10.0),
               child: Text(
-                "Info",
+                _textInfo,
                 textAlign: TextAlign.center,
                 style: TextStyle(color: Colors.green, fontSize: 25.0),
               ),
@@ -47,18 +85,21 @@ class _HomeState extends State<Home> {
               decoration: InputDecoration(
                   labelText: "Peso (Kg)",
                   labelStyle: TextStyle(color: Colors.green)),
+              controller: weightController,
             ),
             TextField(
               keyboardType: TextInputType.number,
               decoration: InputDecoration(
                   labelText: "Altura (cm)",
                   labelStyle: TextStyle(color: Colors.green)),
+              controller: heightController,
             ),
             Container(
-              padding: EdgeInsets.only(left: 0.0, top: 10.0, right: 0.0, bottom: 0.0),
+              padding: EdgeInsets.only(
+                  left: 0.0, top: 10.0, right: 0.0, bottom: 0.0),
               height: 60.0,
               child: RaisedButton(
-                onPressed: () {},
+                onPressed: _calculate,
                 child: Text(
                   "Calcular",
                   style: TextStyle(color: Colors.white, fontSize: 25.0),
